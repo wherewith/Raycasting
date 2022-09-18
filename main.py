8c251d93b2781b2fd72e850ef9427e7a1e893f2d
Kunal Jain <55034053+wherewith@users.noreply.github.com>
2022-09-18 17:51:20 -0500

    encapsulated shade function

--- main.py
+++ main.py
@@ -109,18 +109,23 @@ def draw3D(player, curr_map, curr_ray, ray_angle, ray_dist, num_rays):
     line_height = min((curr_map.scale * 320/max(1, ray_dist)), 320)
     line_width = SCREEN_WIDTH/2 / num_rays
 
-    # black and white shader
-    line_color_unit = 1/(max(1, ray_dist * ray_dist * 0.000106)) * 255
-    if ray_dist <= 1:
-        line_color_unit = 0
-    line_color = (line_color_unit, line_color_unit, line_color_unit)
+    wall_color = shade(c_red, ray_dist)
 
     wall_rect = pygame.Rect(SCREEN_HEIGHT+curr_ray*line_width, (SCREEN_HEIGHT/2) - (line_height/2), line_width +1, line_height)
-    pygame.draw.rect(screen, line_color, wall_rect)
     ceiling_rect = pygame.Rect(SCREEN_HEIGHT+curr_ray*line_width, 0, line_width+1, (SCREEN_HEIGHT/2) - (line_height/2))
-    pygame.draw.rect(screen, c_aqua, ceiling_rect)
     floor_rect = pygame.Rect(SCREEN_HEIGHT+curr_ray*line_width, (SCREEN_HEIGHT/2) + (line_height/2) - 1, line_width+1, (SCREEN_HEIGHT/2) - (line_height/2)+1)
+
+    pygame.draw.rect(screen, c_aqua, ceiling_rect)
+    pygame.draw.rect(screen, wall_color, wall_rect)
     pygame.draw.rect(screen, c_gray, floor_rect)
+
+
+def shade(color, dist):
+    shade_unit = 1 / (max(1, dist ** 2 * 0.00005))
+    if dist <= 1:
+        shade_unit = 1
+    shaded_color = (color.r*shade_unit, color.g*shade_unit, color.b*shade_unit)
+    return shaded_color
 
 
 def draw_window():
