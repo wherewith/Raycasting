36221c37d6ed16e836c8a87537c17a7c5b2a48de
Kunal Jain <55034053+wherewith@users.noreply.github.com>
2022-09-19 22:36:02 -0500

    pyautogui upload

--- run.py
+++ run.py
@@ -2,12 +2,15 @@ import os
 import pathlib
 import zipfile
 import easygui
+import pyautogui
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 def zip_build():
@@ -60,7 +63,10 @@ def upload(game_id):
     except:
         print("Nothing to delete! ")
 
-    upload_button = driver.find_element(by=By.XPATH, value="//div[@class = 'button add_file_btn has_multi_upload']")
+    upload_present = EC.presence_of_element_located((By.XPATH, "//div[@class = 'button add_file_btn has_multi_upload']"))
+    WebDriverWait(driver, 10).until(upload_present).click()  # This opens the windows file selector
+    pyautogui.write(os.getcwd()+'/build/web-release.zip')
+    pyautogui.press('enter')
 
 
 def main():
